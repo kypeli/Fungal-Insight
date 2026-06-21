d954d5b1b7dda2ac687cb67d756081a32a09d6ed
Johan Paul <johan.paul@nitor.com>
2026-06-21 10:07:26 +0300

    Add theme configuration and editorconfig

---
+++ app/src/main/java/com/kypeli/mushrooms/ui/theme/Color.kt
@@ -0,0 +1,32 @@
+package com.kypeli.mushrooms.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MossGreen = Color(0xFF334F2B)
+val MossGreenContainer = Color(0xFF4A6741)
+val OnMossGreen = Color(0xFFFFFFFF)
+val OnMossGreenContainer = Color(0xFFC2E4B4)
+
+val Terracotta = Color(0xFF8F4C34)
+val TerracottaContainer = Color(0xFFFFA98B)
+val OnTerracotta = Color(0xFFFFFFFF)
+val OnTerracottaContainer = Color(0xFF793B24)
+
+val MushroomBeige = Color(0xFF534537)
+val MushroomBeigeContainer = Color(0xFF6C5D4D)
+val OnMushroomBeige = Color(0xFFFFFFFF)
+val OnMushroomBeigeContainer = Color(0xFFECD7C3)
+
+val Parchment = Color(0xFFFAFAF5)
+val OnParchment = Color(0xFF1A1C19)
+
+val SurfaceDim = Color(0xFFDADAD5)
+val SurfaceBright = Color(0xFFFAFAF5)
+val SurfaceContainerLowest = Color(0xFFFFFFFF)
+val SurfaceContainerLow = Color(0xFFF4F4EF)
+val SurfaceContainer = Color(0xFFEEEEE9)
+val SurfaceContainerHigh = Color(0xFFE8E8E3)
+val SurfaceContainerHighest = Color(0xFFE3E3DE)
+
+val Outline = Color(0xFF73796F)
+val OutlineVariant = Color(0xFFC3C8BD)
